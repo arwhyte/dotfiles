ad70bf1929128d838140b2347d77f7acdaf21f1c
Anthony Whyte <anthwhyte@gmail.com>
2016-06-30 10:03:26 -0400

    Add zsh directory and aliases.zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,25 @@
+# navigation aliases
+
+# standard folders
+alias dev='cd ~/Development'
+alias docs='cd ~/Documents'
+
+# code repos
+alias bitrepos='cd ~/Development/repos/bitbucket/'
+alias gitrepos='cd ~/Development/repos/github/'
+alias svnrepos='cd ~/Development/repos/svn/'
+
+# code repos by account
+alias arwhyte='cd ~/Development/repos/github/arwhyte'
+alias ims='cd ~/Development/repos/github/IMSGlobal'
+alias scripts='cd ~/Development/repos/github/arwhyte/scripts'
+
+# rstudio
+alias rstudio='LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server: open -a RStudio'
+
+# tomcat
+alias tomcat='cd /opt/tomcat'
+alias tomcatlogs='cd /opt/tomcat/logs'
+
+# zsh
+alias ohmy='cd ~/.oh-my-zsh'
